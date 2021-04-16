d29f01b709f14713be387f46cac2b69d302b6d19
Lauren S <60661474+lsab094@users.noreply.github.com>
2021-04-16 19:01:08 -0400

    Add files via upload

---
+++ Dates.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author surfacepro3
+ */
+public class Dates {
+    
+    private static Connection connection;
+    private static ArrayList<Date> dates = new ArrayList<Date>();
+    private static PreparedStatement addDate;
+    private static PreparedStatement getDateList;
+    private static ResultSet resultSet;
+    
+    public static void addDate(Date date)
+    {
+        connection = DBConnection.getConnection();
+        try
+        {
+            addDate = connection.prepareStatement(
+                    "insert into date (date) values (?)");
+            addDate.setDate(1, date);
+            addDate.executeUpdate();
+        }
+        catch(SQLException sqlException)
+        {
+            sqlException.printStackTrace();
+        }
+        
+    }
+    
+    public static ArrayList<Date> getDateList()
+    {
+        connection = DBConnection.getConnection();
+        ArrayList<Date> dates = new ArrayList<Date>();
+        try
+        {
+            getDateList = connection.prepareStatement(
+                    "select date from dates order by date");
+            resultSet = getDateList.executeQuery();
+            
+            while(resultSet.next())
+            {
+                dates.add(resultSet.getDate(1));
+            }
+        }
+        catch(SQLException sqlException)
+        {
+            sqlException.printStackTrace();
+        }
+        return dates;
+        
+    }
+    
+}
